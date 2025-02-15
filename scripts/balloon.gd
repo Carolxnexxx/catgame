0f9f359d238d10dc9412cd00f69bdb89341ccc69
Carolxnexxx <caroline.chan0923@gmail.com>
2025-02-15 14:46:54 -0500

    tutorial

--- scripts/balloon.gd
+++ scripts/balloon.gd
@@ -4,6 +4,11 @@ var player_nearby: bool = false
 @export var animation_name: String = "move"
 @onready var animation: AnimationPlayer = $AnimationPlayer
 
+var start_position: Vector2 = Vector2(85, -3)
+
+func _ready():
+	position = start_position  # Ensure balloon starts at correct position
+
 func _on_body_entered(_body: Node2D) -> void:
 	player_nearby = true
 	animation.set_active(true)
--- scripts/exit.gd
+++ scripts/exit.gd
@@ -1,4 +1,16 @@
 extends Node2D
 
-func _on_body_entered(_body: Node) -> void:
-	GameManager.next_level()
+var players_touching := 0
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("players"):
+		players_touching += 1
+		check_players()
+
+func _on_body_exited(body: Node) -> void:
+	if body.is_in_group("players"):
+		players_touching -= 1
+
+func check_players() -> void:
+	if players_touching >= 2:
+		GameManager.next_level()
