5f0c7251481a9ea1d46265cfc1eb490b382c538c
Carolxnexxx <caroline.chan0923@gmail.com>
2025-02-14 23:55:32 -0500

    updates

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -3,14 +3,17 @@ extends Node
 var score = 0
 var current_level = 1
 var level_path = "res://scenes/levels/"
-@onready var score_label = $ScoreLabel
+@onready var score_label_2: Label = $ScoreLabel2
+@onready var score_label_3: Label = $ScoreLabel3
 
 func add_point():
 	score += 1
-	score_label.text = str(score)
+	score_label_2.text = str(score)
+	score_label_3.text = str(score)
 
 func next_level():
 	current_level += 1
+	score = 0
 	var full_path = level_path + "level" + str(current_level) + ".tscn"
 	get_tree().change_scene_to_file(full_path)
 	print("Player entered next level")
